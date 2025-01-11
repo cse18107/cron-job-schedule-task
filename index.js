6200bafb89bf5cbcb2102826ae72b40fe809280f
cse18107 <cse18107@cemk.ac.in>
2025-01-11 18:37:51 +0530

    added schedule

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import cron from 'node-cron';
 
 const exportData = () => {
-    
+    const name = "Report 1";
+    console.log(`Exported ${name}`)
 }
+
+const cronExpression = "*/5 * * * * *";
+const isValid = cron.validate(cronExpression);
+console.log(`is expression valid ${isValid}`);
+
+cron.schedule(cronExpression, () => {
+    exportData();
+}, {
+    scheduled: false
+})
